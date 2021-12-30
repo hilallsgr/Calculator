cee62d578dfcf1561cb392139a2d166f413b3a81
hilallsgr <96839055+hilallsgr@users.noreply.github.com>
2021-12-30 20:20:39 +0300

    Update calculator.java
    add to method

--- src/calculator.java
+++ src/calculator.java
@@ -2,4 +2,7 @@ public class calculator {
     public static int hesapla(int s1,int s2){
         return s1+s2;
     }
+    public static int hesapla2(int sayi1,int sayi2){
+        return sayi1-sayi2;
+    }
 }
